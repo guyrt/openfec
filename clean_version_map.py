dac56fd421a6e75a168dc98a98a190002d63e94e
Tommy Guy <riguy@microsoft.com>
2015-11-11 15:06:57 -0800

    Name and tab cleanup. Yikes!

--- clean_version_map.py
+++ clean_version_map.py
@@ -8,17 +8,17 @@ fec_map = dict()
 
 
 def proc_row(row):
-	version = row[0]
-	form = row[1]
-	if version not in fec_map:
-		fec_map[version] = dict()
-	if form not in fec_map[version]:
-		fec_map[version][form] = []
-	for x in range(2, len(row)):
-		rowelt = row[x]
-		if not pd.isnull(row[x]):
-			_, name = rowelt.split('-', 1)
-			fec_map[version][form].append(name)
+    version = row[0]
+    form = row[1]
+    if version not in fec_map:
+        fec_map[version] = dict()
+    if form not in fec_map[version]:
+        fec_map[version][form] = []
+    for x in range(2, len(row)):
+        rowelt = row[x]
+        if not pd.isnull(row[x]):
+            _, name = rowelt.split('-', 1)
+            fec_map[version][form].append(name)
 
 
 df = pd.read_excel("fec_version_map.xlsx", sheetname="all versions")
--- fecparser.py
+++ fecparser.py
@@ -2,39 +2,39 @@ from json import loads
 
 
 class FecFileParser(object):
-	"""
-	Given a file from the FEC, apply correct definitions.
-	"""
+    """
+    Given a file from the FEC, apply correct definitions.
+    """
 
-	def __init__(self, definitions):
-		self.feclookup = loads(definitions.read())
+    def __init__(self, definitions):
+        self.feclookup = loads(definitions.read())
 
-	def getschema(self, version, linetype):
-		versioned_formdata = self.feclookup['v' + version]
-		i = len(linetype) - 1
-		while i >= 0 and w[:i] not in d:
-			i -= 1
-		if not linetype:
-			raise Exception("Could not match linetype {0} on version {1}".format(linetype, version))
-		return versioned_formdata[linetype]
+    def getschema(self, version, linetype):
+        versioned_formdata = self.feclookup['v' + version]
+        i = len(linetype) - 1
+        while i >= 0 and w[:i] not in d:
+            i -= 1
+        if not linetype:
+            raise Exception("Could not match linetype {0} on version {1}".format(linetype, version))
+        return versioned_formdata[linetype]
 
-	def processfile(self, filehandle):
-		"""
-		Process all lines of a file and list of dictionaries, one per line.
-		"""
-		first_line = filehandle.readline().strip().split(chr(28))
-		if first_line[0] != "HDR":
-			raise Exception("Failed to parse: HDR expected on first line")
+    def processfile(self, filehandle):
+        """
+        Process all lines of a file and list of dictionaries, one per line.
+        """
+        first_line = filehandle.readline().strip().split(chr(28))
+        if first_line[0] != "HDR":
+            raise Exception("Failed to parse: HDR expected on first line")
 
-		fileversion = first_line[2]
-		
-		for line in filehandle:
-			line = line.strip.split(chr(28))
-			linetype = line[0]
-			schema = self.getschema(fileversion, linetype)
+        fileversion = first_line[2]
+        
+        for line in filehandle:
+            line = line.strip.split(chr(28))
+            linetype = line[0]
+            schema = self.getschema(fileversion, linetype)
 
-			# TODO - F3 lines should be used to produce a dictionary of information about each organization. Must be second in every file.
+            # TODO - F3 lines should be used to produce a dictionary of information about each organization. Must be second in every file.
 
-			yield {k: v for k, v in zip(schema, line)}
+            yield {k: v for k, v in zip(schema, line)}
 
 
