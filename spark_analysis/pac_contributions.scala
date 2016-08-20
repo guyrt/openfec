69f268e55f5f2bc5e5419f7d5fa7b58782a5217d
Tommy Guy <richardtguy84@gmail.com>
2016-08-19 21:01:54 -0700

    added pac contribution script

--- spark_analysis/pac_contributions.scala
+++ spark_analysis/pac_contributions.scala
@@ -14,7 +14,6 @@ val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val records = sqlContext.jsonFile("wasb://fecfilings@openfecdata.blob.core.windows.net/raw_filings/2016*.json")
 
 var sa_contributions = records
-    .where((lower($"FORM_TYPE") === "sa11c") ||(lower($"FORM_TYPE") === "sa11b"))
     .where($"CONTRIBUTION_AMOUNT_{F3L_Bundled}" > 0)
 
 sa_contributions = sa_contributions
@@ -27,22 +26,5 @@ val contributionsByDonor = sa_contributions
     .groupBy("DONOR_COMMITTEE_FEC_ID", "DONOR_CANDIDATE_FEC_ID", "FILER_COMMITTEE_ID_NUMBER")
     .agg(sum("CONTRIBUTION_AMOUNT_{F3L_Bundled}"), count("CONTRIBUTION_AMOUNT_{F3L_Bundled}"))
 
-
 val output = "wasb://fecfilings@openfecdata.blob.core.windows.net/processed_output/PAC_by_candidate.json";
 contributionsByDonor.repartition(1).write.mode(SaveMode.Overwrite).format("json").save(output)
-
-// this is for exploration:
-
-// get committees
-var committees = sqlContext.jsonFile("wasb://fecfilings@openfecdata.blob.core.windows.net/raw_mappings/committee.json")
-
-// get candidates.
-var candidates = sqlContext.jsonFile("wasb://fecfilings@openfecdata.blob.core.windows.net/raw_mappings/candidate.json")
-
-val candidatesWithPCC = candidates.where($"CAND_PCC" !== "")
-
-val contributionsWithCandidate = contributionsByDonor
-    .join(candidatesWithPCC, 
-        contributionsByDonor("FILER_COMMITTEE_ID_NUMBER") === candidatesWithPCC("CAND_PCC"), "left_outer")
-
-contributionsWithCandidate.groupBy("CAND_OFFICE").count()
